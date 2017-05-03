2963c28aa4ae9d7088d195f03c99b43bea03878d
ChristopherForsman <ch719forsman@gmail.com>
2017-05-03 08:57:01 +0300

    första gången portfoliet blev uppladdat till github

---
+++ kontakt.php
@@ -0,0 +1,77 @@
+<!doctype html>
+
+<html lang="sv">
+<head>
+  <meta charset="utf-8">
+
+  <title>Christopher Forsman</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Christopher Forsman">
+
+
+  <link rel="stylesheet" href="css/styles.css">
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+ 
+ 
+</head>
+
+<body id="contact">
+
+
+	<ul>
+	  <li><a href="./index.html">Hem</a></li>
+	  <li><a href="musik.html">Musik</a></li>
+	  <li><a href="projekt.html">Projekt</a></li>
+
+	  <li><a href="tjänster.html">Tjänster</a></li>
+	  <li><a href="kontakt.html">Kontakt</a></li>
+	</ul>
+
+	<div class="img-background test">
+		<h1 class="p"><strong>Kontaka mig!</strong></h1>
+		<br>
+	</div>
+<div class="container">
+<div class="row">
+<div class="col">
+<p>Skicka e-post</p>
+<form>
+  <div class="form-group">
+    <label for="exampleInputName">Förnamn</label>
+    <input type="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Förnamn">
+
+  </div>
+    <div class="form-group">
+    <label for="exampleInputName">Efternamn</label>
+    <input type="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Efternamn">
+
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail">Password</label>
+    <input type="email" class="form-control" id="exampleInputPassword1" placeholder="Email">
+  </div>
+
+
+  <div class="form-group">
+    <label for="exampleTextarea">Meddelande</label>
+    <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+  </div>
+
+
+
+  <button type="submit" class="btn btn-primary">Skicka</button>
+</form>
+</div>
+<div class="col callBox">
+	<p>Ring mig<br></p><img src="img/kontakt.jpg" class="img-fluid" alt="telefon">
+
+	<a href="callto:0452087130" class="btn btn-primary">Ring</a>
+</div><div class="col">
+<p>Besök mig<br></p><img src="img/map.bmp" class="img-fluid" alt="map">
+<label for="exampleTextarea"><strong><br>Besök mig på adressernas:</strong> <br><br> Forsby Byväg 40, Nykarleby<br>Hummelholmsvägen 3 Nykarleby (Monäs)</label>
+	
+</div>
+	
+  <script src="js/scripts.js"></script>
+</body>
+</html>
